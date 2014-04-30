0149073e5e6cfbc9f8d734d79a757f3ff360f0f4
Lisnichenko Dmitro <dlysnichenko@hortonworks.com>
2014-04-30 19:03:35 +0300

    fix

--- freshtmp.py
+++ freshtmp.py
@@ -15,13 +15,13 @@ GIT_CLEAN_CMD = ["git", "clean", "-df"]
 
 
 def prepare_repo_dir():
-  os.chdir(backup_repo_dir)
   if os.path.isfile(backup_repo_dir):
     # Stupid situation
     raise IOError("Repo dir {0} is a file?!!".format(backup_repo_dir))
   if not os.path.isdir(backup_repo_dir):
-    print "Creating repo dir {0}...".format(backup_repo_dir)
+    print "Creating repo dir {0}".format(backup_repo_dir)
     os.makedirs(backup_repo_dir)
+  os.chdir(backup_repo_dir)
   if not os.path.isdir(git_dir):
     print "Initializing new git repository at {0}".format(backup_repo_dir)
     subprocess.check_call(GIT_INIT_CMD)
