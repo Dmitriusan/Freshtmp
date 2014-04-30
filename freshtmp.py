e4dfe5326eff4991a8fec1064f464f636abbbfb8
Lisnichenko Dmitro <dlysnichenko@hortonworks.com>
2014-04-30 19:47:14 +0300

    draft

--- freshtmp.py
+++ freshtmp.py
@@ -1,15 +1,20 @@
 #!/usr/bin/env python
 import os, subprocess
+import shutil
+import time
 
 work_dir = "/tmp"
 backup_repo_dir = "/media/hotspot/backups/freshtmp_repo"
-git_dir = os.path.join(backup_repo_dir, ".git")
+patch_extensions = [".patch", ".diff"]
 
 
 #####
 
 GIT_INIT_CMD = ["git", "init"]
 GIT_CLEAN_CMD = ["git", "clean", "-df"]
+GIT_ADD_CMD = ["git", "add", "."]
+GIT_COMMIT_CMD = ["git", "commit", "-a"]
+git_dir = os.path.join(backup_repo_dir, ".git")
 
 #####
 
@@ -27,9 +32,48 @@ def prepare_repo_dir():
     subprocess.check_call(GIT_INIT_CMD)
   subprocess.check_call(GIT_CLEAN_CMD)
 
+def move_files():
+  print "Working..."
+  for directory, dirnames, filenames in os.walk(work_dir):
+    for f in filenames:
+      ext = os.path.splitext(f)
+      if ext[1] in patch_extensions:
+        print "!!! {0}/{1}".format(directory, f)
+        try:
+          abs_path = os.path.join(directory, f)
+          move(abs_path)
+        except Exception, e:
+          print "Can not move file {0} : {1}".format(abs_path, e.message)
+    # try to remove directory if it is empty
+    try:
+      os.rmdir(directory)
+    except OSError:
+      # ignore
+      pass
+
+def move(file_path):
+  # Extracts subpath from the working dir to a given path
+  subpath = os.path.relpath(os.path.dirname(file_path), os.path.abspath(work_dir))
+  # new file location
+  new_dir = os.path.join(backup_repo_dir, subpath)
+  print "Moving {0} to {1}".format(file_path, new_dir)
+  if not os.path.exists(new_dir):
+    os.makedirs(new_dir)
+  #shutil.move(file_path, new_dir)
+  shutil.copy(file_path, new_dir)
+
+def commit():
+  print "Committing..."
+  # Adding commit message
+  message =time.strftime("Automatic commit on %d/%m/%Y %H:%M:%S")
+  cmd = GIT_COMMIT_CMD + ["-m", message]
+  subprocess.check_call(cmd)
+
 
 def main():
   prepare_repo_dir()
+  move_files()
+  commit()
 
 if __name__ == "__main__":
   main()
